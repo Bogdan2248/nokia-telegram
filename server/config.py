8b331b474d31656c46e0e1b8155d2474b0530e95
Bogdan <bogdan@Bogdan>
2026-04-28 20:44:47 +0500

    0

--- server/config.py
+++ server/config.py
@@ -2,7 +2,7 @@
 # API credentials from https://my.telegram.org
 # Вставьте ваши реальные данные ниже
 
-API_ID = 0  # Ваш API ID (число)
-API_HASH = 'ВАШ_API_HASH'  # Ваш API Hash (строка)
+API_ID = 34235172 # Ваш API ID (число)
+API_HASH = 'f24bd53a37c46606e117e77d62fb3808'  # Ваш API Hash (строка)
 
 PORT = 5000
